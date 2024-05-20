7b2cab3df9e17194e60970978b604eab32cfba5c
Damilola Adebowale <adebzdamzy@gmail.com>
2024-05-20 12:16:26 +0100

    final commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,13 +7,13 @@ export default function Nav() {
 
   return (
     <>
-    <div id='m-id' className='flex top-0 gap-5 justify-between fixed z-[100] text-sm text-100  w-full shadow-md align-middle py-3 pr-10 pl-4 animate-[opacity_5s_ease-in-out] bg-white'>
+    <div id='m-id' className='flex top-0 gap-5 justify-between fixed z-[100] text-[0.7rem] text-100  w-full shadow-md align-middle py-3 pr-4 sm:pr-10 pl-4 animate-[opacity_5s_ease-in-out] bg-white'>
         <Link to='/home'>
         <div className="w-[2em] h-[2em] ">   
         <img target="_blank" src={icon} className="  text-[0.7em] object-cover shadow-sm " />
         </div>
         </Link>
-        <div className='flex gap-4 lg:gap-11  lg:text-[1rem] font-semibold font-sans '> 
+        <div className='flex gap-4 lg:gap-11  sm:text-[1rem] font-semibold font-sans '> 
             <h2 className=' cursor-pointer'>
             <Link to='/about'>About us
               </Link>
